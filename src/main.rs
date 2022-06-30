0960c44932a2ce8e3d9c45671594475f12ef2ee8
Mark Crouch <macevic040@hotmail.co.uk>
2022-06-30 20:33:39 +0100

    Add multiple Users to vector then output them.

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,42 @@ struct User {
     age: u16,
 }
 
+impl User {
+    // TODO: Add a print User method
+}
+
 fn main() {
     println!("Hello, world!");
     let my_user = User {
         username: String::from("Mark Crouch"),
         age: 57,
     };
-    println!("My user's name is: {}",
-        my_user.username);
-    println!("My user's age is: {}",
-        my_user.age);
+    // println!("My user's name is: {}",
+    //     my_user.username);
+    // println!("My user's age is: {}",
+    //     my_user.age);
     
     // Vector
     let mut users: Vec<User> = Vec::new();
     users.push(my_user);
+
+    // Add another user (add straigth to Users vector)
+    users.push(User { 
+        username: (String::from("Bob Smith")), 
+        age: (52) 
+    });
     
-    match users.get(0) {
-        Some(vec_user) => {
-            println!("Name: {}",vec_user.username);
-            println!("Age: {}", vec_user.age);
-        }
-        None => println!("Error: Missing Element!!"),
+    // match users.get(0) {
+    //     Some(vec_user) => {
+    //         println!("Name: {}",vec_user.username);
+    //         println!("Age: {}", vec_user.age);
+    //     }
+    //     None => println!("Error: Missing Element!!"),
+    // }
+
+    for user in users {
+        println!("-------------------");
+        println!("Name: {}", user.username);
+        println!("Age: {}", user.age);
     }
 }
